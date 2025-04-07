4556652b94ad7fde13302bf9a288b791d4ad9503
Eduardo Chaves <duduchaves98@gmail.com>
2025-04-07 19:44:33 -0300

    First Commit

---
+++ src/main/java/br/com/edu/RestWithSpringBootErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootErudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootErudioApplication.class, args);
+    }
+
+}
