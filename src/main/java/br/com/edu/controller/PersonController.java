872bd773b7a2653dbc55a2d453d6b33562a40aff
Eduardo Chaves <duduchaves98@gmail.com>
2025-04-11 17:34:53 -0300

    commit da aula

--- src/main/java/br/com/edu/controller/PersonController.java
+++ src/main/java/br/com/edu/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,12 @@ public class PersonController {
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonDTO findById(@PathVariable("id") Long id) {
-        return personServices.findById(id);
+        var person = personServices.findById(id);
+        person.setBirthDate(new Date());
+        person.setPhoneNumber("");
+        person.setLastName(null);
+        person.setSensitiveData("bla bla bla");
+        return person;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/br/com/edu/data/dto/v1/PersonDTO.java
+++ src/main/java/br/com/edu/data/dto/v1/PersonDTO.java
@@ -1,19 +1,43 @@
 package br.com.edu.data.dto.v1;
 
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import br.com.edu.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
+//@JsonPropertyOrder({"id","first_name","last_name","address","gender"})
+@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
+//    @JsonProperty("first_name")
     private String firstName;
+//    @JsonProperty("last_name")\
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phoneNumber;
     private String address;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date birthDate;
+//    @JsonIgnore
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
+
+    private String sensitiveData;
 
     public PersonDTO() {
     }
@@ -58,15 +82,40 @@ public class PersonDTO implements Serializable {
         this.gender = gender;
     }
 
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSensitiveData() {
+
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(address, personDTO.address) && Objects.equals(gender, personDTO.gender);
+        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(phoneNumber, personDTO.phoneNumber) && Objects.equals(address, personDTO.address) && Objects.equals(birthDate, personDTO.birthDate) && Objects.equals(gender, personDTO.gender) && Objects.equals(sensitiveData, personDTO.sensitiveData);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, address, gender);
+        return Objects.hash(id, firstName, lastName, phoneNumber, address, birthDate, gender, sensitiveData);
     }
 }
